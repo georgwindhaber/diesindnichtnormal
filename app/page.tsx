4a12a7286555ee3efe642c16f946afb144a6d462
Georg Windhaber <georg@journiapp.com>
2023-07-15 14:07:43 +0200

    Add tailwind

--- app/page.tsx
+++ app/page.tsx
@@ -4,16 +4,8 @@ import "./styles.css";
 
 export default async function Homepage() {
   const scandals = await reader.collections.scandals.all();
-
   return (
-    <div>
-      <h1>Keystatic ⚡️</h1>
-      <p>This homepage shows how to load a collection from the reader API.</p>
-      <p>
-        <a href="/keystatic">Click here to visit the Admin UI</a>, or the link
-        below to view a post in the collection.
-      </p>
-      <h2>Posts</h2>
+    <main>
       <ul>
         {scandals.map((scandal) => (
           <li key={scandal.slug}>
@@ -21,6 +13,6 @@ export default async function Homepage() {
           </li>
         ))}
       </ul>
-    </div>
+    </main>
   );
 }
